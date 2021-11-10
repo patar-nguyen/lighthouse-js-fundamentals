3e66b5b6e64c8949c5161219e55bc13c2f857f30
Patrick Nguyen <patar.nguyen@gmail.com>
2021-11-10 14:44:10 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,24 @@
+/*const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
 const temperature = 12;
 
 if (temperature < 0) {
@@ -8,4 +29,12 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
