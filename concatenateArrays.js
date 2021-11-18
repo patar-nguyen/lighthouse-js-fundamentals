1bf25a402097a7492330cf8c3dcaa81c315f8146
Patrick Nguyen <patar.nguyen@gmail.com>
2021-11-17 20:28:05 -0500

    Concatenate Arrays

---
+++ concatenateArrays.js
@@ -0,0 +1,15 @@
+let concat = function(firstArray, secondArray){
+  let concatenated = [];
+  for(let first of firstArray){
+    concatenated.push(first);
+  }
+  for(let second of secondArray){
+    concatenated.push(second);
+  }
+  return concatenated;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
