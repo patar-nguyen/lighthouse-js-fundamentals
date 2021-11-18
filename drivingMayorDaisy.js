4a30854b4d1e7bc9da4ae8749c30297a3d7945fd
Patrick Nguyen <patar.nguyen@gmail.com>
2021-11-17 20:28:50 -0500

    Driving Mayor Daisy

---
+++ drivingMayorDaisy.js
@@ -0,0 +1,27 @@
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40
+  },
+  {
+    time: 1568329821632,
+    speed: 42
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const carPassing = function (cars, speed) {
+  let object = {
+    time: Date.now(),
+    speed: speed
+  }
+  cars.push(object);
+  return cars;
+};
+
+const speed = 38
+  
+console.log(carPassing(cars, speed));
