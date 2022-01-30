bf62bfd7772e8915f5d52468a28694bdff47a306
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-30 17:09:46 -0500

    xMarksThePerfectShot

--- xMarksThePerfectShot.js
+++ xMarksThePerfectShot.js
@@ -16,25 +16,3 @@ const finalPosition = function(moves){
   return position;
 }
 console.log(finalPosition(moves));
-
-/*
-const finalPosition = function(moves){
-  let position = [0, 0];
-  for(let i = 0; i < moves.length; i++){
-    if(moves[i] === 'north'){
-      position[1] += 1;
-    }
-    else if (moves[i] === 'south'){
-      position[1] -= 1; 
-    }
-    else if (moves[i] === 'west'){
-      position[0] -= 1;
-    }
-    else if (moves[i] === 'east'){
-      position[0] += 1;
-    }
-  }
-  return position;
-}
-
-console.log(finalPosition(moves));*/
